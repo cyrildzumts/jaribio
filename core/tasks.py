f761369e76aed4ddefc8f9276f65706a5dd7a9c8
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-09-15 22:17:48 +0200

    WIP

--- core/tasks.py
+++ core/tasks.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 @shared_task
 def send_mail_task(email_context=None):
-    
+    logger.info("Send mail task called")
     # TODO : make sending email based on Django Template System.
     if email_context is not None and isinstance(email_context, dict):
         logger.debug("email_context available. Running send_mail now")
@@ -36,5 +36,7 @@ def send_mail_task(email_context=None):
         )
     else:
         logger.warn(f"send_mail_task: email_context missing or is not a dict. email_context : {email_context}")
+
+    logger.info("Send mail task finished")
 
     
