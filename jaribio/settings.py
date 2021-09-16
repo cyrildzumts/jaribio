985303bdf8fee0a76293f3e5e48a6ffa51178860
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-09-16 19:08:45 +0200

    WIP

--- jaribio/settings.py
+++ jaribio/settings.py
@@ -95,6 +95,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'api.apps.ApiConfig',
     'accounts',
     'quiz.apps.QuizConfig',
     'core.apps.CoreConfig',
