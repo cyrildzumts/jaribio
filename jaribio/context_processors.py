cca4761ba86608f8d55201f341fb385c0321838d
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-09-16 17:30:05 +0200

    WIP

--- jaribio/context_processors.py
+++ jaribio/context_processors.py
@@ -6,8 +6,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 def site_context(request):
-    is_dashboard_allowed = False
-    cart_items_count = 0
     if request.user.is_authenticated:
         
         is_dashboard_allowed = request.user.has_perm('dashboard.can_view_dashboard')
@@ -18,9 +16,7 @@ def site_context(request):
         'META_KEYWORDS': settings.META_KEYWORDS,
         'META_DESCRIPTION': settings.META_DESCRIPTION,
         'redirect_to' : '/',
-        'is_dashboard_allowed' : is_dashboard_allowed,
         'dev_mode' : settings.DEV_MODE,
-        'ALLOW_GOOGLE_ANALYTICS' : settings.ALLOW_GOOGLE_ANALYTICS,
         'CURRENCY' : settings.CURRENCY,
         'next_url' : request.path,
     }
