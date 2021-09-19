49324433ef09470ac4fa3bda66b1c446d7b36008
Cyrille Ngassam Nkwenga <cyrildz@gmail.com>
2021-09-19 22:42:36 +0200

    WIP

--- api/urls.py
+++ api/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('create-quiz/', views.create_quiz, name="create-quiz"),
     path('quizzes/<uuid:quiz_uuid>/create-question/', views.create_question, name="create-question"),
     path('quizzes/<uuid:quiz_uuid>/create-quizstep/', views.create_quizstep, name="create-quizstep"),
-    path('quizzes/<uuid:quiz_slug>/questions/<uuid:question_uuid>/update/', views.update_question, name="update-question"),
+    path('quizzes/<slug:quiz_slug>/questions/<uuid:question_uuid>/update/', views.update_question, name="update-question"),
     #path('quizzes/<uuid:quiz_slug>/questions/<uuid:question_uuid>/', views.question_details, name="question-details"),
     path('user-search/', views.UserSearchView.as_view(), name="user-search"),
 ]
